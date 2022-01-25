b6d1c0594d151abdf8a59d3ac98b885d31114699
rhit-brunkocl <brunkocl@rose-hulman.edu>
2022-01-25 06:14:27 -0500

    UI done, star class created

---
+++ app/src/main/java/edu/rosehulman/stargaze/Star.kt
@@ -0,0 +1,5 @@
+package edu.rosehulman.stargaze
+
+data class Star(var RA:String, var Dec:String, var sep:Float, var delta_sep:Float, var magnitude:Float, var delta_mag:Float, var num_obs:Int, var last_year_obs:Int){
+
+}
